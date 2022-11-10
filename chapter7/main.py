2f7435c79ab8f8b19f681f55f43936bf41edbaf1
Ivan Kuchin <ivan.kuchin@gmail.com>
2022-11-10 00:43:54 +0000

    code clean-up

--- chapter7/main.py
+++ chapter7/main.py
@@ -24,57 +24,6 @@ from keras.layers import Dropout
 from matplotlib import pyplot
 
 
-# # define the standalone discriminator model
-# def define_discriminator(in_shape=(28, 28, 1)):
-#     model = Sequential()
-#     model.add(Conv2D(64, (3, 3), strides=(2, 2), padding='same', input_shape=in_shape))
-#     model.add(LeakyReLU(alpha=0.2))
-#     model.add(Dropout(0.4))
-#     model.add(Conv2D(64, (3, 3), strides=(2, 2), padding='same'))
-#     model.add(LeakyReLU(alpha=0.2))
-#     model.add(Dropout(0.4))
-#     model.add(Flatten())
-#     model.add(Dense(1, activation='sigmoid'))
-#     # compile model
-#     opt = Adam(learning_rate=0.0002, beta_1=0.5)
-#     model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
-#     return model
-#
-#
-# # define the standalone generator model
-# def define_generator(latent_dim):
-#     model = Sequential()
-#     # foundation for 7x7 image
-#     n_nodes = 128 * 7 * 7
-#     model.add(Dense(n_nodes, input_dim=latent_dim))
-#     model.add(LeakyReLU(alpha=0.2))
-#     model.add(Reshape((7, 7, 128)))
-#     # upsample to 14x14
-#     model.add(Conv2DTranspose(128, (4, 4), strides=(2, 2), padding='same'))
-#     model.add(LeakyReLU(alpha=0.2))
-#     # upsample to 28x28
-#     model.add(Conv2DTranspose(128, (4, 4), strides=(2, 2), padding='same'))
-#     model.add(LeakyReLU(alpha=0.2))
-#     model.add(Conv2D(1, (7, 7), activation='sigmoid', padding='same'))
-#     return model
-#
-#
-# # define the combined generator and discriminator model, for updating the generator
-# def define_gan(d_model, g_model):
-#     # make weights in the discriminator not trainable
-#     d_model.trainable = False
-#     # connect them
-#     model = Sequential()
-#     # add generator
-#     model.add(g_model)
-#     # add the discriminator
-#     model.add(d_model)
-#     # compile model
-#     opt = Adam(learning_rate=0.0002, beta_1=0.5)
-#     model.compile(loss='binary_crossentropy', optimizer=opt, metrics=["accuracy"])
-#     return model
-
-
 def define_discriminator():
     inputs = tf.keras.Input(shape=(28, 28, 1), name="inputs")
     conv1 = tf.keras.layers.Conv2D(64, kernel_size=(3, 3), strides=(2, 2), padding="same", name="conv1")(inputs)
