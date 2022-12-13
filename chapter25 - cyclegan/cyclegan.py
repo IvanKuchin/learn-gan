b4189156c72c0425dd2f339dd49369a643be3842
Ivan Kuchin (ikuchin) <ikuchin@cisco.com>
2022-12-12 21:40:48 -0500

    chapter 25: improve quality of generated pictures

--- chapter25 - cyclegan/cyclegan.py
+++ chapter25 - cyclegan/cyclegan.py
@@ -22,6 +22,7 @@ def save_images(images, name):
     edge = int(np.sqrt(images.shape[0]))
     min, max = images.min(), images.max()
     images = (images - min) / (max - min)
+    pyplot.figure(figsize=(20, 20), dpi=240)
     for i in range(edge**2):
         pyplot.subplot(edge, edge, i+1)
         pyplot.axis(False)
